b33773eca1cf6b9b7309334ccfe0fee7fbba6f99
nhatjollie <nguyendnhat12@gmail.com>
2024-10-12 01:39:52 +1100

    Edited localhost and added CorsRegistry

---
+++ src/main/java/swin/swe4006/c6g1/config/WebConfig.java
@@ -0,0 +1,17 @@
+package swin.swe4006.c6g1.config; // Adjust the package as necessary
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // React app's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
